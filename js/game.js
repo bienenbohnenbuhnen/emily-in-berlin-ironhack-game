1c09c2068ab54bc502bf212693c3725181c94f19
Andy Cockroft <acockroft1994@gmail.com>
2023-06-12 16:23:38 +0200

    moving background

---
+++ js/game.js
@@ -0,0 +1,44 @@
+class Game {
+  constructor() {
+    this.startScreen = document.getElementById("game-start-screen");
+    this.gamePort = document.getElementById("game-port");
+    this.gameOverScreen = document.getElementById("game-over");
+    this.player = null;
+    this.height = 500;
+    this.width = 1000;
+    this.obstacles = [];
+    this.powerUps = [];
+    this.score = 0;
+    this.lives = 4;
+    this.gameIsOver = false;
+  }
+
+  start() {
+    this.gamePort.style.width = `${this.width}px`;
+    this.gamePort.style.height = `${this.height}px`;
+    this.startScreen.style.display = "none";
+    this.gamePort.style.display = "block";
+
+  }
+
+  gameLoop() {
+    console.log("game looping");
+
+    if (this.gameIsOver) {
+        return;
+    }
+
+    this.update();
+
+    window.requestAnimationFrame(() => this.gameLoop());
+  }
+  
+
+
+
+  update() {
+    console.log("in the update");
+  }
+
+  endGame() {}
+}
